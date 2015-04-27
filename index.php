d6526ad89b713c3fba43dedd9f78e45713328688
MartinShishkov <martin.shishkov@hotmail.com>
2015-04-27 17:08:45 +0300

    Added separation for controller, method and parameters

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
 
-var_dump($_SERVER);
+define("DX_ROOT_DIR", dirname(__FILE__) . '/');
+define("DX_ROOT_PATH", basename(dirname(__FILE__)) . '/');
+
+$request = $_SERVER["REQUEST_URI"];
+$request_home = '/' . DX_ROOT_PATH;
+
+$controller = "main";
+$method = "index";
+$param = array();
+
+if(!empty($request)){
+    if( strpos($request, $request_home) === 0){
+        $request = substr($request, strlen($request_home));
+
+        $components = explode('/', $request, 3);
+
+        var_dump($request);
+        var_dump($components);
+
+        if(count($components) > 1){
+            list($controller, $method) = $components;
+
+            if(isset($components[2])){
+                $param = $components[2];
+            }
+        }
+    }
+}
+
+var_dump($controller);
+var_dump($method);
+var_dump($param);
